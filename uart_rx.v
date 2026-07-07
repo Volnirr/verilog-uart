3ae840563cf09783a57354472e4af806fa1aca0b
Liam Gillaspy <101990817+Volnirr@users.noreply.github.com>
2026-07-07 13:24:36 +0000

    clean up comments

--- uart_rx.v
+++ uart_rx.v
@@ -3,7 +3,8 @@ module reciever (
 	input ftdi_txd, // reciever
 	output reg [7:0] led
 );
-
+	
+	// synchronizer
 	reg [1:0] rx_sync = 2'b11;
 	always @(posedge clk_25mhz) rx_sync <= {rx_sync[0], ftdi_txd};
 	wire rx = rx_sync[1];
@@ -26,18 +27,17 @@ module reciever (
 			state <= START;
 		end
 		if (state != IDLE) begin
-			counter <= counter + 1;
+			counter <= counter + 1; 
 			case (state)
 				START: begin
 					if (counter >= 108) begin
 						counter <= 0;
-						state <= rx ? IDLE : DATA;
-
+						state <= rx ? IDLE : DATA; // verify bit is still low
 					end
 				end
 				DATA: begin
 					if (counter >= 217) begin
-						data <= {rx, data[7:1]};
+						data <= {rx, data[7:1]}; // shift LSB out
 						bitcount <= bitcount + 1;
 						if (bitcount == 7) begin
 							state <= STOP;
